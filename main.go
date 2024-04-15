dae6cb0f315c555ec83342066c3f03066aee3b5a
Siddhant Vij <siddhantvij201@gmail.com>
2024-04-15 09:44:32 +0530

    Ch 1: Servers

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func readiness(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	corsMux := corsMiddleware(mux)
+
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readiness)
+
+	log.Fatal(http.ListenAndServe(":8080", corsMux))
 }
